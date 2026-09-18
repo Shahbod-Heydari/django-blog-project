9cbd08ea450ab863cb18a994cad49af4648cc942
shahbod <shahbodhey84@gmail.com>
2026-09-18 21:08:51 +0330

    feat: make posts dynamic and add blog content

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('',views.index,name='starting-page'),
     path('posts', views.post, name='post-page'),
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,93 @@
+from datetime import date
 from django.shortcuts import render
 
 # Create your views here.
 
+all_posts = [
+    {
+        'slug': 'learning-django',
+        'title': 'Django course',
+        'author': 'Shahbod',
+        'image': 'django.jpg',
+        'date':date(2026,9,18),
+        'short_description': 'special python framework',
+        'content': '''
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at dignissimos dolores, 
+        doloribus ducimus eaque, eius fugiat in ipsam libero magnam maiores neque perferendis 
+        placeat quae recusandae repudiandae similique voluptate.
+        ''',
+    },
+    {
+        'slug': 'learning-git',
+        'title': 'git course',
+        'author': 'Mohammad',
+        'image': 'git.jpg',
+        'date':date(2024,9,18),
+        'short_description': 'version control system',
+        'content': '''
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at dignissimos dolores, 
+        doloribus ducimus eaque, eius fugiat in ipsam libero magnam maiores neque perferendis 
+        placeat quae recusandae repudiandae similique voluptate.
+        ''',
+    },
+    {
+        'slug': 'learning-python',
+        'title': 'Python course',
+        'author': 'Shahbod',
+        'image': 'python.jpg',
+        'date':date(2020,9,18),
+        'short_description': 'programming language',
+        'content': '''
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at dignissimos dolores, 
+        doloribus ducimus eaque, eius fugiat in ipsam libero magnam maiores neque perferendis 
+        placeat quae recusandae repudiandae similique voluptate.
+        ''',
+    },
+    {
+        'slug': 'learning-cpp',
+        'title': 'c++ course',
+        'author': 'Hossein',
+        'image': 'c++.jpg',
+        'date':date(2025,9,18),
+        'short_description': 'programming language',
+        'content': '''
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at dignissimos dolores, 
+        doloribus ducimus eaque, eius fugiat in ipsam libero magnam maiores neque perferendis 
+        placeat quae recusandae repudiandae similique voluptate.
+        ''',
+    },
+    {
+        'slug': 'learning-potgreSQL',
+        'title': 'postgreSQL course',
+        'author': 'Shahbod',
+        'image': 'postgresql.png',
+        'date':date(2021,9,18),
+        'short_description': 'relational database management system',
+        'content': '''
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at dignissimos dolores, 
+        doloribus ducimus eaque, eius fugiat in ipsam libero magnam maiores neque perferendis 
+        placeat quae recusandae repudiandae similique voluptate.
+        ''',
+    }
+]
+
+def get_date(post):
+    return post['date']
+
 def index(request):
-    return render(request, 'blog/index.html')
+    sorted_posts = sorted(all_posts,key = get_date)
+    latest_post = sorted_posts[-2:] # use for contex arg
+    return render(request, 'blog/index.html',{'latest_post':latest_post})
 
 def post(request):
-    return render(request, 'blog/all-posts.html')
+    context = {
+        'all_posts': all_posts
+    }
+    return render(request, 'blog/all-posts.html',context)
 
 def single_post(request,slug):
-    return render(request, 'blog/post-details.html')
+    post = next(post for post in all_posts if post['slug'] == slug)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post-details.html',context)
