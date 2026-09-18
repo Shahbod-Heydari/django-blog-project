0143aa52e9692260aba567128b04314510e30d78
shahbod <shahbodhey84@gmail.com>
2026-09-18 13:52:59 +0330

    feat: add all posts page and improve template structure

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='starting-page'),
-    path('post', views.post, name='post-page'),
+    path('posts', views.post, name='post-page'),
     path('post/<slug:slug>', views.single_post, name='post-detail-page')
 ]
